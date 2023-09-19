7da42489e9e5fed2eef0bce53a751c093c4c1cf0
Chris Kruining <chris@kruining.eu>
2023-09-19 05:38:30 +0200

    trying smithay-egui

---
+++ launcher/src/integration.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use smithay::{
+    backend::{
+        renderer::{
+            element::{texture::TextureRenderElement, Element, RenderElement},
+            gles::GlesTexture,
+            glow::GlowRenderer,
+            Frame, Renderer,
+        },
+        winit,
+    },
+    input::{
+        keyboard::{FilterResult, XkbConfig},
+        pointer::{AxisFrame, ButtonEvent, MotionEvent},
+        SeatHandler, SeatState,
+    },
+    utils::{Rectangle, Transform, SERIAL_COUNTER},
+};
+use smithay_egui::EguiState;
+
+
+impl SeatHandler for State {
+    type KeyboardFocus = EguiState;
+    type PointerFocus = EguiState;
+    fn seat_state(&mut self) -> &mut SeatState<Self> {
+        &mut self.seat_state
+    }
+}
+
+struct State {
+    seat_state: SeatState<State>,
+}
+
+pub fn integrate() -> Result<()> {
+    // create a winit-backend
+    let (mut backend, mut input) = winit::init::<GlowRenderer>().map_err(|_| anyhow::anyhow!("Winit failed to start"))?;
+
+    let egui = EguiState::new(Rectangle::from_loc_and_size(
+        (0, 0),
+        backend.window_size().physical_size.to_logical(1),
+    ));
+
+    Ok(())
+}
--- launcher/src/lib.rs
+++ launcher/src/lib.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
+mod integration;
 
 pub use app::App;
